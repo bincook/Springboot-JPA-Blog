dc0562b296ef412220668606e9dc22624e13a879
김영빈 <bincook902@gmail.com>
2022-05-20 15:46:33 +0900

    환경세팅 완료

---
+++ src/main/java/com/example/blog/test/BlogController.java
@@ -0,0 +1,15 @@
+package com.example.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		
+		return "<h1>hello blog</h1>";
+	}
+}
